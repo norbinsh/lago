1f92feba8f50790f1c24603b5e23bb40f508fb49
shay elmualem <wowshay@gmail.com>
2020-03-21 11:43:33 +0200

    meet opswork reqs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const url = require('url');
 const isSubS = require('./src/utils/isSubSequence');
-// create a simple server object
-http.createServer(function(req, res) {
+const port = 80
+
+
+app.get('/', function (req, res) {
   const queryObject = url.parse(req.url,true).query;
   const subSorNot = isSubS.isSubsequence(queryObject.str1, queryObject.str2).toString();
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`is ${queryObject.str1} subsequence of ${queryObject.str2}? ${subSorNot.toUpperCase()}`); // response to client
-  res.end(); // end the reponse
-}).listen(3000);
+  res.send(`is ${queryObject.str1} subsequence of ${queryObject.str2}? ${subSorNot.toUpperCase()}`);
+} )
+
+app.listen(port, () => console.log(`Lago app listening on port ${port}, running using OpsWorks!`))
